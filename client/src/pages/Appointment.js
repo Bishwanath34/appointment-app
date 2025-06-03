4e26fa41a57d95610679c4277c08e457a158bc67
Bishwanath34 <91818190+Bishwanath34@users.noreply.github.com>
2025-06-03 11:29:37 -0700

    Update Appointment.js

--- client/src/pages/Appointment.js
+++ client/src/pages/Appointment.js
@@ -13,7 +13,7 @@ function Appointment() {
           const getAppointmentsData=async()=>{
       try{
       dispatch(showloading());  
-      const response = await axios.get('https://appointment-app-2.onrender.com/api/user/get-appointments-by-user-id', {
+      const response = await axios.get('https://appointment-app-ser.onrender.com/api/user/get-appointments-by-user-id', {
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
